8264d443c43dac07cdf11704db03a64216ec2c8d
hsshin <hsshin@euclidsoft.co.kr>
2024-10-08 18:03:41 +0900

    first blog flutter commit 20241008 shs

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:blog_flutter/screen/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
